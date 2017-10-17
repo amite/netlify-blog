b927f20d7ee1d97710ad3c194654d28be8a48621
amite <amit.erandole@gmail.com>
2017-10-17 19:41:26 +0530

    added list of posts to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      {posts &&
+        posts.map(({ node: post }) => {
+          const { path, title, image } = post.frontmatter
+          return (
+            <div key={path} className="posts">
+              <h1>{title}</h1>
+              {image && <img src={image} />}
+              <Link to={path}>Go to post</Link>
+            </div>
+          )
+        })}
+    </div>
+  )
+}
+
+export const postsQuery = graphql`
+  query allBlogPosts {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+            image
+          }
+          html
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
